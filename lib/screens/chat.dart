fd6d90884da04fa9904464723f3bdb96acc3ac68
RolandMarton <roland111187@gmail.com>
2023-06-13 15:54:23 +0200

    Finish chat app project

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,8 +1,35 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatScreen extends StatelessWidget {
+import 'package:chat_app/widgets/chat_message.dart';
+import 'package:chat_app/widgets/chat_messages.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+
+    // final token = await fcm.getToken();
+    // print(token); // you can send this token (via HTTP or Firestore SDK) to a backend
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +41,18 @@ class ChatScreen extends StatelessWidget {
               onPressed: () {
                 FirebaseAuth.instance.signOut();
               },
-              icon: Icon(Icons.exit_to_app),
+              icon: const Icon(Icons.exit_to_app),
               color: Theme.of(context).colorScheme.primary,
             ),
           ],
         ),
-        body: const Center(
-          child: Text('Logged in!'),
+        body: const Column(
+          children: [
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage(),
+          ],
         ));
   }
 }
